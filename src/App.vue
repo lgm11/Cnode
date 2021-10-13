d6bbf279ef6f3ec8efb0be0ed3bce5234aca25fe
lee <1079811936@qq.com>
2021-10-13 09:26:15 +0800

    完成Header和Postlist组件

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <Header></Header>
+    <div class="main">
+      <Postlist></Postlist>
     </div>
-    <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import Header from './components/Header.vue'
+import Postlist from './components/Postlist.vue'
+export default {
+  components:{
+    Header,Postlist
   }
+}
+</script>
+
+<style lang="scss">
+.main{
+  width: 80%;
+  margin: 0 auto;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="header">
+        <img src="https://static2.cnodejs.org/public/images/cnodejs_light.svg" alt="">
+        <ul>
+            <li><a href="">首页</a></li>
+            <li><a href="">新手入门</a></li>
+            <li><a href="">API</a></li>
+            <li><a href="">关于</a></li>
+            <li><a href="">注册</a></li>
+            <li><a href="">登录</a></li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+.header{
+    background: #5a5555;
+    height: 50px;
+}
+ul{
+    list-style: none;
+    float: right;
+    margin: 4px;
+}
+li{
+    display: inline-block;
+    padding: 10px 15px;
+}
+img{
+    max-width: 120px;
+    margin-left: 50px;
+    margin-top: 10px;
+}
+a{
+    text-decoration: none;
+    color: #ccc;
+    font-size: 14px;
+    text-shadow: none;
+}
+</style>
